a9ff346376bbaff97c740b7581eb198a7f4d8958
Erik Lundmark <cjiqqq@gmail.com>
2016-06-30 12:52:30 +0200

    Hotfix article bug and create Infopage

---
+++ TESS/Models/view_Information.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TietoCRM.Models
+{
+    public class view_Information
+    {
+        int _id;
+        public int _ID { get; set; }
+
+        String title;
+        public string Title { get; set; }
+
+        String message;
+        public String Message { get; set; }
+
+        DateTime _created;
+        public DateTime Created { get; set; }
+
+        DateTime updated;
+        public DateTime Updated { get; set; }
+
+
+    }
+}
