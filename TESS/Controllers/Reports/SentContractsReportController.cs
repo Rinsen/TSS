319b40d73c98392915157d2b9a12c41d46ecb001
Linus Sjöström <linus_sjostrom@hotmail.com>
2016-07-07 16:02:59 +0200

    Fix bug with sent contracts

--- TESS/Controllers/Reports/SentContractsReportController.cs
+++ TESS/Controllers/Reports/SentContractsReportController.cs
@@ -44,32 +44,27 @@ namespace TietoCRM.Controllers.Reports
 
         public List<Dictionary<String, String>> GenerateSentContracts()
         {
-            List<view_Customer> customers = view_Customer.getAllCustomers();
+            List<view_Customer> customers = view_Customer.getAllCustomers(System.Web.HttpContext.Current.GetUser().Sign);
             List<Dictionary<String, String>> rows = new List<Dictionary<String, String>>();
             foreach (view_Customer customer in customers)
             {
-                Dictionary<String, String> dict = new Dictionary<String, String>();
                 int amountValidContracts = 0;
                 List<view_Contract> contracts = view_Contract.GetContracts(customer.Customer);
                 foreach (view_Contract contract in contracts)
                 {
                     if (contract.Status == "Sänt")
                     {
-                        amountValidContracts++;
+                        Dictionary<String, String> dict = new Dictionary<String, String>();
+                        dict.Add("customer", customer.Customer);
+                        dict.Add("customer_type", customer.Customer_type);
+                        dict.Add("representative", customer.Representative);
+                        dict.Add("it_manager", customer.IT_manager);
+                        dict.Add("contract_id", contract.Contract_id);
+                        dict.Add("title", contract.Title);
+                        rows.Add(dict);
                     }
                 }
-                dict.Add("customer", customer.Customer);
-                dict.Add("customer_type", customer.Customer_type);
-                dict.Add("representative", customer.Representative);
-                dict.Add("it_manager", customer.IT_manager);
 
-                List<view_Contract> mainContracts = contracts.Where(c => c.Contract_type == "Huvudavtal" && c.Status == "Sänt" && System.Web.HttpContext.Current.GetUser().IfSameArea(c.Area)).ToList();
-                if (mainContracts.Count > 0)
-                {
-                    dict.Add("main_contract_id", mainContracts[0].Contract_id);
-                    dict.Add("amount", amountValidContracts.ToString());
-                    rows.Add(dict);
-                }
             }
             return rows;
         }
