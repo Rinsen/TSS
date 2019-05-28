7bc69db63073f09a1668b6f82c22894e4025f693
Mattias Franck <Mattias.Franck@explizit.se>
2019-05-28 14:56:40 +0200

    Contextmenyn är efter TinyMCE 5.0 inbyggd i editor core, så vi plockar bort context meny plugin från initieringen av TinyMCE. Säkrar även upp fler tinyMCE.execCommand med try-catch

--- TESS/Scripts/CRMFunctions.js
+++ TESS/Scripts/CRMFunctions.js
@@ -350,7 +350,7 @@ var webkit = get_browser() == "Chrome" ||
 var tinyDefaultPlugins = [
         'advlist autolink lists link image charmap print preview anchor',
         'searchreplace visualblocks code fullscreen',
-        'insertdatetime media table contextmenu paste code'
+        'insertdatetime media table paste code'
     ];
 var tinyDefaultToolbars = 'insertfile undo redo | styleselect         \
     | bold italic | alignleft aligncenter alignright alignjustify |   \
