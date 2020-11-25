b23d397f64eda180e069d276b4e757ec31811089
Mats-I-Nilsson <mats.nilsson@explizit.se>
2020-11-25 09:39:01 +0100

    Fortlöpande ändringar

---
+++ TESS/Controllers/List Management/TemplateMainContract.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using TietoCRM.Models;
+
+namespace TietoCRM.Controllers.List_Management
+{
+    public class TemplateMainContractController : Controller
+    {
+        private String htmlMainTemplate = "";
+        // GET: Template
+        public ActionResult Index()
+        {
+            //List<MainContractText> list = MainContractText.getAllMainContractTexts();
+            //TreeToHtml(list);
+            //ViewData.Add("Templates", htmlMainTemplate);
+            ViewData.Add("TopTitle", MainContractText.GetTitle1());
+            ViewData.Add("Epilog", MainContractText.GetEpilog());
+            ViewData.Add("Prolog", MainContractText.GetProlog());
+            ViewData.Add("ModuleText", MainContractText.GetModuleText());
+            return View();
+        }
+
+        [ValidateInput(false)]
+        public String SaveMainContractText()
+        {
+            try
+            {
+                String epilog = Request.Form["epilog"];
+                String prolog = Request.Form["prolog"];
+                String topTitle = Request.Form["title"];
+                String moduleText = Request.Form["moduleText"];
+
+                try
+                {
+                    MainContractText.Update("Epilog", epilog);
+                    MainContractText.Update("Prolog", prolog);
+                    MainContractText.Update("rubrik1", topTitle);
+                    MainContractText.Update("ModulText", moduleText);
+                }
+                catch (Exception e)
+                {
+                    return "0";
+                }
+
+
+
+                return "1";
+            }
+            catch
+            {
+                return "-1";
+            }
+
+
+        }
+        private void TreeToHtml(List<MainContractText> textList)
+        {
+            foreach (MainContractText item in textList)
+            {
+                if (item.Type == MainContractText.MainContractType.MainHead)
+                {
+                    htmlMainTemplate += "<input data-name='" + item.Name + "' type='text' class='form-control main-template-mainhead' value='" + item.Value + "'/>";
+
+                }
+                else if (item.Type == MainContractText.MainContractType.Subheading)
+                {
+                    htmlMainTemplate += "<input data-name='" + item.Name + "' type='text' class='form-control main-template-subheading' value='" + item.Value.Replace("\t", "") + "'/>";
+                }
+                else if (item.Type == MainContractText.MainContractType.Text)
+                {
+                    htmlMainTemplate += "<textarea data-name='" + item.Name + "' class='form-control main-template-text'>" + item.Value + "</textarea>";
+                }
+                if (item.Children.Count > 0)
+                {
+                    TreeToHtml(item.Children);
+                }
+            }
+        }
+    }
+}
---
+++ TESS/Models/view_TemplateMainContract.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace TietoCRM.Models
+{
+    public class view_MainContractTemplate : SQLBaseClass
+    {
+        public enum MainContractType
+        {
+            MainHead,
+            Subheading,
+            Text
+        };
+
+        private MainContractType type;
+        public MainContractType Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+
+        private String name;
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private String value;
+        public String Value
+        {
+            get { return this.value; }
+            set { this.value = value; }
+        }
+
+        private List<MainContractText> children;
+        public List<MainContractText> Children
+        {
+            get { return children; }
+            set { children = value; }
+        }
+
+        /// <summary>
+        /// init the object with certain values.
+        /// </summary>
+        /// <param name="name">The column name in the database</param>
+        /// <param name="type">The heading type of the object, MainHead, Subheading, Text</param>
+        /// <param name="value">The value the database holds</param>
+        public view_MainContractTemplate(String name, MainContractType type, String value)
+        {
+            this.Name = name;
+            this.Type = type;
+            this.Value = value;
+            this.Children = new List<MainContractText>();
+        }
+        /// <summary>
+        /// Updates the given column name with given value to the database
+        /// </summary>
+        /// <param name="name">The column name in the database</param>
+        /// <param name="value">The value you want for that given column name in the database</param>
+        public static void Update(String name, String value)
+        {
+
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["DataBaseCon"].ConnectionString))
+            using (SqlCommand command = new SqlCommand(String.Format("UPDATE dbo.view_MainContractTemplate SET {0}=@value", name), connection))
+            {
+                connection.Open();
+
+                /*SqlParameter nameParam = new SqlParameter("@name", System.Data.SqlDbType.NVarChar, 100);
+                nameParam.Value = name;*/
+                SqlParameter valueParam = new SqlParameter("@value", System.Data.SqlDbType.NVarChar, -1);
+                valueParam.Value = value;
+               
+                
+                //command.Parameters.Add(nameParam);
+                command.Parameters.Add(valueParam);
+                command.Prepare();
+                int a = command.ExecuteNonQuery();
+
+
+              
+
+
+            }
+        }
+
+        /// <summary>
+        /// Get all the main contract templates from the database and parse it in to a list of objects of this class
+        /// </summary>
+        /// <returns>Returns a list of MainContractText</returns>
+ 
+        public static String GetEpilog()
+        {
+            String epilog = "";
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["DataBaseCon"].ConnectionString))
+            using (SqlCommand command = connection.CreateCommand())
+            {
+                connection.Open();
+
+
+                // Default query
+                command.CommandText = "SELECT Epilog FROM dbo.Havtalsmall";
+
+                command.Prepare();
+
+
+                command.ExecuteNonQuery();
+
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        if (reader.HasRows)
+                        {
+                            epilog = reader["Epilog"].ToString();
+                        }
+                    }
+                }
+            }
+            return epilog;
+        }
+        public static String GetProlog()
+        {
+            String prolog = "";
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["DataBaseCon"].ConnectionString))
+            using (SqlCommand command = connection.CreateCommand())
+            {
+                connection.Open();
+
+
+                // Default query
+                command.CommandText = "SELECT Prolog FROM dbo.Havtalsmall";
+
+                command.Prepare();
+
+
+                command.ExecuteNonQuery();
+
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        if (reader.HasRows)
+                        {
+                            prolog = reader["Prolog"].ToString();
+                        }
+                    }
+                }
+            }
+            return prolog;
+        }
+
+        public static String GetTitle1()
+        {
+            String title = "";
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["DataBaseCon"].ConnectionString))
+            using (SqlCommand command = connection.CreateCommand())
+            {
+                connection.Open();
+
+
+                // Default query
+                command.CommandText = "SELECT rubrik1 FROM dbo.Havtalsmall";
+
+                command.Prepare();
+
+
+                command.ExecuteNonQuery();
+
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        if (reader.HasRows)
+                        {
+                            title = reader["rubrik1"].ToString();
+                        }
+                    }
+                }
+            }
+            return title;
+        }
+        public static String GetModuleText()
+        {
+            String text = "";
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["DataBaseCon"].ConnectionString))
+            using (SqlCommand command = connection.CreateCommand())
+            {
+                connection.Open();
+
+
+                // Default query
+                command.CommandText = "SELECT ModulText FROM dbo.Havtalsmall";
+
+                command.Prepare();
+
+
+                command.ExecuteNonQuery();
+
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        if (reader.HasRows)
+                        {
+                            text = reader["ModulText"].ToString();
+                        }
+                    }
+                }
+            }
+            return text;
+        }
+    }
+}
